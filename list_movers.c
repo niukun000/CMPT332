b43d492a78e25836f1589f13db96887d26960161
Kun Niu (kun869) <kun869@mail.usask.ca>
2020-09-20 16:53:14 -0600

    Upload New File

---
+++ list_movers.c
@@ -0,0 +1,35 @@
+/*
+Kun Niu
+kun869
+11242427
+*/
+
+/*
+Boru sun, 
+bos226, 
+11233036
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include list.h
+
+void *ListNext(list){
+    printf("Got to procedure ListNext")
+}
+void *ListPrev(list){
+printf("Got to procedure ListPrev")
+}
+void *ListFirst(list){
+printf("Got to procedure ListFirst")
+}
+void *ListLast(list){
+printf("Got to procedure ListLast")
+}
+void *ListCurr(list){
+printf("Got to procedure ListCurr")
+}
+void *ListSearch(list, comparator, comparisonArg){
+printf("Got to procedure ListSearch")
+}
