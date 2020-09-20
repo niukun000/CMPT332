6c1dd9369787eb97ca2b2a23eb81bce36ca3e7ff
Kun Niu (kun869) <kun869@mail.usask.ca>
2020-09-20 16:01:32 -0600

    Upload New File

---
+++ PartA.c
@@ -0,0 +1,22 @@
+
+/* NAME:
+   NSID:
+   Student Number:11242427
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct thread {
+}
+
+int thread_create(){}
+
+int thread_sleep(){}
+
+int thread_join(){}
+int parse_args(){}
+
+int main(args, argv[]){
+	parse_args()
+
+}
