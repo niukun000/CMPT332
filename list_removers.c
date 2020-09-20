5e96126fb9aa68242441a27459ac29bf1378d65e
Kun Niu (kun869) <kun869@mail.usask.ca>
2020-09-20 16:53:24 -0600

    Upload New File

---
+++ list_removers.c
@@ -0,0 +1,26 @@
+/*
+Kun Niu
+kun869
+11242427
+*/
+
+/*
+Boru sun, 
+bos226, 
+11233036
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include list.h
+
+void *ListRemove(list){
+printf("Got to procedure ListRemove")
+}
+void ListFree(list, itemFree){
+printf("Got to procedure ListFree")
+}
+void *ListTrim(list){
+printf("Got to procedure ListTrim")
+}
