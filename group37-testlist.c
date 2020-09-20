d228e6fa91c8817d7dee2198f0f09cc30349ae98
Kun Niu (kun869) <kun869@mail.usask.ca>
2020-09-20 16:59:23 -0600

    Upload New File

---
+++ group37-testlist.c
@@ -0,0 +1,30 @@
+/*
+Kun Niu
+kun869
+11242427
+*/
+
+/*
+Boru sun, 
+bos226, 
+11233036
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "List.h"
+
+int main(int argc, char const *argv[])
+{
+    printf("create list")
+    list1 = ListCreate()
+    printf("ListAppend")
+    
+    ListAppend(list, 2)
+    printf("ListAdd")
+    ListAdd(list, 3)
+
+    return 0;
+}
+
+ 
