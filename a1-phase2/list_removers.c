f865064ed79f6138f76bb280e478f8bae2ca632c
Boru Sun (bos226) <bos226@mail.usask.ca>
2020-09-28 08:09:59 -0600

    Upload list_removers.c

---
+++ a1-phase2/list_removers.c
@@ -0,0 +1,26 @@
+/*
+Kun Niu
+kun869
+11242427
+*/
+
+/*
+Boru sun, 
+bos226, 
+11233036
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "list.h"
+
+void *ListRemove(List *list){
+printf("Got to procedure ListRemove");
+}
+void ListFree(List *list, void* itemFree){
+printf("Got to procedure ListFree");
+}
+void *ListTrim(List *list){
+printf("Got to procedure ListTrim");
+}
